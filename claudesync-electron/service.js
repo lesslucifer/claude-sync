34491a8beb1ca93d35aaee94d48bfdb93971f940
Vu Luu <quangvu.luu92@gmail.com>
2024-07-23 23:48:04 +0700

    * fix serv & icon for macos

--- claudesync-electron/service.js
+++ claudesync-electron/service.js
@@ -2,9 +2,10 @@ const { spawn, fork } = require('node:child_process')
 const path = require('path')
 
 fork(path.join(process.cwd(), "node_modules", "electron", "cli.js"), ["."], {
-    shell: true,
+    shell: false,
     detached: true,
-    windowsHide: false
+    windowsHide: false,
+    stdio: 'ignore'
 }).unref()
 
 process.exit()
